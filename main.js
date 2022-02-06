b179aeab77d56977a0ca29885678be61b7b3dcc2
pawan-live <pawan.senpura@gmail.com>
2022-02-06 17:53:45 +0530

    added cookie functionality

--- main.js
+++ main.js
@@ -1,9 +1,52 @@
-let username = prompt("Please enter your name");
-let group = prompt("What is your group? (4.1/4.2)");
+var username, group;
+const date = findDate();
 
-if (username != null) {
+//Check for cookies
+let cookies = checkCookies();
+if (cookies == false) {
+  //get username and group
+  getUsernameAndGroup();
   setCookie("username", username, 120);
   setCookie("group", group, 120);
+  // console.log("cookies set successfully");
+}
+
+username = getCookie("username");
+group = getCookie("group");
+// console.log("Cookies retrieved successfully");
+
+// display name & group
+document.getElementById("username").innerHTML = "Hello " + username + "!";
+document.getElementById("group").innerHTML = group;
+
+// display date month day
+document.getElementById("date").innerHTML =
+  "It's " + date[0] + ", " + date[1] + " " + date[2];
+
+/* FUNCTIONS */
+
+//Check cookies
+function checkCookies() {
+  if (document.cookie == "") {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+//Get Username and Group
+function getUsernameAndGroup() {
+  do {
+    username = prompt("Enter your nick name");
+    if (username == null || username == "") {
+      continue;
+    } else {
+      group = prompt("Enter your semester group (4.1/4.2)");
+      if (group != "4.1" || group != "4.2") {
+        continue;
+      }
+    }
+  } while (username == null || username == "");
 }
 
 // Set a Cookie
@@ -14,19 +57,62 @@ function setCookie(cName, cValue, expDays) {
   document.cookie = cName + "=" + cValue + "; " + expires + "; path=/";
 }
 
-// Apply setCookie
-setCookie("username", username, 30);
-setCookie("group", group, 30);
-
-// console.log(document.cookie);
-
-cookies = document.cookie;
-if (document.cookie == "") {
-  console.log("null");
-} else {
-  console.log(cookies);
+//Get a cookie
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
 
-// delete cookie
-// document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
-// document.cookie = "group=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+// Clear cookies
+function clearCookies() {
+  document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+  document.cookie = "group=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+  location.reload();
+}
+
+// Get date, month, day to an array
+function findDate() {
+  const dateArray = [];
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const d = new Date();
+
+  dateArray[0] = weekday[d.getDay()];
+  dateArray[1] = month[d.getMonth()];
+  dateArray[2] = d.getDate();
+  return dateArray;
+}
